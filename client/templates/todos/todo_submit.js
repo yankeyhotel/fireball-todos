b959642f6332e776cb78de250ab11d51c71577fa
Matt McClard <yankeyhotel@gmail.com>
2015-01-06 12:36:21 -0600

    Add user accounts. Remove insecure. And create routes / access denied templates.

---
+++ client/templates/todos/todo_submit.js
@@ -0,0 +1,32 @@
+Template.todoSubmit.events({
+	'submit form': function(e){
+		e.preventDefault();
+
+		var todo = {
+			title: 		$(e.target).find('[name=title]').val(),
+			duedate: 	new Date( $(e.target).find('[name=duedate]').val() )
+		}
+
+		todo._id = Todos.insert(todo);
+		Router.go('todoPage', todo);
+		
+	}
+})
+
+
+Template.todoSubmit.rendered = function() {
+
+	// http://eonasdan.github.io/bootstrap-datetimepicker/#options
+	$('#duedate').datetimepicker({
+		pickDate: 		true,					// en/disables the date picker
+		pickTime: 		true,					// en/disables the time picker
+		useMinutes: 	true,					// en/disables the minutes picker
+		useSeconds: 	false,					// en/disables the seconds picker
+		useCurrent: 	false,					// when true, picker will set the value to the current date/time
+		minuteStepping: 30, 					// set the minute stepping
+    	minDate: 		moment().format('L'),	// set a minimum date
+    	showToday: 		true,					// shows the today indicator
+    	sideBySide: 	true, 					// show the date and time picker side by side
+	});
+
+}
